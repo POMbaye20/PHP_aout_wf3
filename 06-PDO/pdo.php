893e0e32e14b59a23350f91c51b7ec6a6de3027c
Papaoumar <39194180+POMbaye20@users.noreply.github.com>
2018-08-21 14:12:01 +0200

    commit 06
    c 06 pdo avec table en HTML, bindParam

--- 06-PDO/pdo.php
+++ 06-PDO/pdo.php
@@ -103,5 +103,142 @@ echo $employe->prenom . '<br>';
 // Attention : après une requête, il faut choisir l'un des fetch(). Si l'on veut en refaire un, il faut refaire la requête : en effet, on ne peut pas effectuer plusieurs transformations successives sur le même objet $result.
 
 
+// ------------------------
+// Exercice : afficher le service de l'employé dont l'id_employe est 417 (production)
+
+$result = $pdo->query("SELECT * FROM employes WHERE id_employes = 417");
+$employe  = $result->fetch(PDO::FETCH_OBJ);  
+debug($employe);
+echo $employe->service . '<br>'; 
+
+// ou
+
+$result = $pdo->query("SELECT service FROM employes WHERE id_employes = 417");
+$employe = $result->fetch(PDO::FETCH_ASSOC);    // on transforme l'objet $result (qui n'est pas exploitable directement) en un array associatif avec pour indice le nom du champ du SELECT(ici service)
+debug($employe);    // on voit ici le contenu de l'array associatif
+
+echo $employe['service'] . '<br>';
+
+// Si la requête retourne qu'un seul résultat => pas de boucle. Si elle peut potentiellement en retourner plusieurs => boucle.
 
 
+// -----------------------------------------------------
+// 04- fetch() avec boucle while (plusieurs résultats)
+// -----------------------------------------------------
+echo '<h3>04- fetch() avec boucle while (plusieurs résultats) </h3> ';
+
+$resultat = $pdo->query("SELECT * FROM employes"); /// cette requête retourne plusieurs résultats, on fait donc une boucle pour les parcourir.
+
+
+echo 'Nombre d\'employés : ' . $resultat->rowCount() . '<br>';  // permet de compter le nombre de lignes retournées par le SELECT (exemples : nombre de produits séllectionnés dans une boutique)
+
+// Comme nous avons plusieurs lignes de résultats, nous devons faire une boucle while : 
+while ($employe = $resultat->fetch(PDO::FETCH_ASSOC)) { // fetch() retourne la ligne SUIVANTE du jeu de résultat en un array associatif. La boucle while permet de faire avancer le curseur dans le jeu de résultat et s'arrête quand le curseur est arrivé à la fin.
+    // debug($employe); // $employe est un array associatif qui contient des données d'un seul employé à chaque tour de boucle
+
+    echo '<div>';
+        echo $employe['prenom'] . '<br>';
+        echo $employe['nom'] . '<br>';
+        echo $employe['service'] . '<br>'; 
+    echo '------------</div>';  
+}
+
+// Attention : il n'y a pas UN array avec tous les enregistrements dedans, mais un array par enregistrement, un array par employé !
+
+
+// -----------------------------------------------------
+// 05- Exercice
+// -----------------------------------------------------
+echo '<h3>05- Exercice </h3> ';
+
+// Afficher la liste des différents service dans une liste <ul><li>.
+
+
+$resultat = $pdo->query("SELECT DISTINCT service FROM employes");       // fonctionne aussi ioci avec GROUP BY, mais ce dernier est plutôt utilisé avec les fonctions qui assemblent des éléments comme SUM(), COUNT(), MIN(), MAX() ET AVG()
+
+echo '<ul>';
+    while($employe = $resultat->fetch(PDO::FETCH_ASSOC)) {
+        echo '<li>'. $employe['service'] . '</li>'; 
+    }
+echo '</ul>';
+
+
+// -----------------------------------------------------
+//  06- fetchAll()
+// -----------------------------------------------------
+echo '<h3> 06- fetchAll() </h3> ';
+
+$resultat = $pdo->query("SELECT * FROM employes");
+debug($resultat);   
+
+$donnees = $resultat->fetchAll(PDO::FETCH_ASSOC);   // retourne TOUTES les lignes de résultats dans un tableau multidimensionnel (sans faire de boucle) : nous avons 1 array associatif par employé à chaque indice numérique.
+
+debug($donnees);    // array mulitidemsionnel
+
+echo '<hr>';
+
+// Pour afficher son contenu, on fait une boucle foreach :
+ foreach ($donnees as $employe) {
+     //debug($employe);   // $employe correspond aux sous arrays qui représentent un employé à chaque tour de boucle
+
+     echo "<div>
+                <p>$employe[prenom]</p>
+                <p>$employe[nom]</p>
+                <p>$employe[salaire]</p>
+        </div><hr>";
+
+}
+
+// Si nous avions voulu afficher TOUTES les infos de façon dynamique, nous aurions fait 2 foreach imbriquées l'une dans l'autre.
+
+
+// -----------------------------------------------------
+//  07- Table HTML
+// -----------------------------------------------------
+echo '<h3> 07- Table HTML </h3> ';
+
+// On veut afficher dynamiquement les résultats de la requête sous forme de table HTML.
+
+$resultat = $pdo->query("SELECT id_employes, prenom, nom, service, salaire FROM employes ORDER BY salaire DESC");
+
+echo '<table border="1">';
+    // La ligne d'entêtes : 
+    echo '<tr>';
+        echo '<th>id_employés</th>';
+        echo '<th>prénom</th>';
+        echo '<th>nom</th>';
+        echo '<th>service</th>';
+        echo '<th>salaire</th>';
+    echo '</tr>';
+
+    // Affichage des autres lignes :
+    while ($ligne = $resultat->fetch(PDO::FETCH_ASSOC)) {
+        echo '<tr>';
+            // Affichage des infos de chaque ligne $ligne : 
+            foreach ($ligne as $info) {
+                echo '<td>' . $info . '</td>';
+            } 
+        echo '</tr>';
+    }
+echo '</table>';
+
+
+// -----------------------------------------------------
+//  08- Requête préparée et bindParam()
+// -----------------------------------------------------
+echo '<h3> 08- Requête préparée et bindParam() </h3> ';
+
+$nom = 'sennard';
+
+// 1- Préparer la requête : 
+$resultat = $pdo->prepare("SELECT * FROM employes WHERE nom = :nom" );  // :nom est un marqueur nominatif qui attend qu'on lui donne une valeur
+
+// 2- Lier les marqueurs aux valeurs :
+$resultat->bindParam(':nom', $nom); // 
+
+// 3- Exécuter la requête :
+$resultat->execute();
+
+// 4- Affichage
+$donnees = $resultat->fetch(PDO::FETCH_ASSOC);
+debug($donnees);
