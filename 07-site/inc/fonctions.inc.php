6670321c6f49b05cc6335455f80a6eb818f922cf
Papaoumar <39194180+POMbaye20@users.noreply.github.com>
2018-08-22 17:01:09 +0200

    commit 09
    c 09

--- 07-site/inc/fonctions.inc.php
+++ 07-site/inc/fonctions.inc.php
@@ -13,7 +13,11 @@ function  debug($param) {
 
 // Fonction qui indique si l'internaute est connecté :
 function internauteEstConnecte() {
-    
-
-
+    if(isset($_SESSION['membre'])) {    // si la session "membre"" existe, c'est que l'internaute est passé par la page de connexion et que nous avons créé cet indice dans $_SESSION.
+        return true;
+    } else {
+        return false;
+    }
+    // OU
+    return (isset($_SESSION['membre']));
 }
